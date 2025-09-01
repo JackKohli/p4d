50cdd03678ad9d8d65bb8fcf1c2210f60b975cec
jack <jackohli@hotmail.com>
2025-09-01 15:31:44 +0100

    control flow exercise

--- control_flow.py
+++ control_flow.py
@@ -1,6 +1,16 @@
-age = 29
+# age = 29
 
-if age > 30:
-    print("you're getting old")
+# if age > 30:
+#     print("you're getting old")
+# else:
+#     print("you're the right side of 30")
+
+grade = int(input("what was your score? "))
+if grade >= 90:
+    print("A grade")
+elif grade >= 80:
+    print("B grade")
+elif grade >= 70:
+    print("C grade")
 else:
-    print("you're the right side of 30")
+    print("Fail")
